a0b99c13036a6cfb8629c416da4cc76cb841c7fb
Chris Kenny <159801689+Chriswkenny@users.noreply.github.com>
2026-02-15 23:44:51 -0500

    Add files via upload

---
+++ Ecosystem.java
@@ -0,0 +1,37 @@
+package edu.monmouth.ecosystem;
+
+public class Ecosystem{
+
+	    public static void main(String[] args) {
+	        River river = new River(20);
+	       	        
+	        //check if bear eats fish and bears create new bear
+	        river.addAnimal(new Bear(), 3);
+	        river.addAnimal(new Bear(), 19);
+	        river.addAnimal(new Bear(), 14);
+	        river.addAnimal(new Fish(), 17);
+	        river.addAnimal(new Fish(), 20);
+	        river.addAnimal(new Fish(), 4);
+	        river.addAnimal(new Bear(), 7);
+	        river.addAnimal(new Bear(), 9);
+	        river.addAnimal(new Fish(), 1);
+	        river.addAnimal(new Fish(), 2);
+	        river.addAnimal(new Fish(), 8);
+	        
+	        System.out.println("Initial River:");
+	        river.display();
+	        
+	        
+	        for (int step = 1; step <= 5; step++) {
+	            river.moveAnimal();
+	            System.out.println("Step " + step + ":");
+	            river.display();
+	        }
+	    }
+	}
+	    
+
+
+
+	
+
